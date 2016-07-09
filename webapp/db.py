80b2b414e2fcb778c9d1b80b23eddd5bc4196a78
Michał Krzysztof Feiler <archiet@platinum.edu.pl>
2016-07-09 14:27:31 +0200

    Working on finding stations and ranges for highstock.com charts

--- webapp/db.py
+++ webapp/db.py
@@ -12,23 +12,59 @@ db = client['trmstac']
 c = db['allsta']
 
 
+def findlatest():
+    return c.find({}, {"_id": 0}).limit(1).sort("timestamp", -1)
+
+
 def najnowszy():
-    nasz = c.find({}, {"_id": 0}).limit(1).sort("timestamp", -1)
-    return [k for k in nasz]
+    return [k for k in findlatest()]
 
 
-def dajod(odtstamp):
-    start = datetime.datetime.fromtimestamp(odtstamp)
+def findall():
+    nasz = c.find({}, {"_id": 0}).sort("timestamp", 1)
+    return nasz
+
+
+def findod(start):
+    if start is None:
+        return findall()
     print start
     nasz = c.find({'timestamp': {'$gte': start}},
                   {"_id": 0}).sort("timestamp", 1)
-    return [k for k in nasz]
+    return nasz
 
 
 def dajoddo(odtstamp, dotstamp):
-    start = datetime.datetime.fromtimestamp(odtstamp)
-    stop = datetime.datetime.fromtimestamp(dotstamp)
+    nasz = findoddo(
+        datetime.datetime.fromtimestamp(
+            odtstamp) if odtstamp is not None else None,
+        datetime.datetime.fromtimestamp(
+            dotstamp) if dotstamp is not None else None,
+    )
+    return [k for k in nasz]
+
+
+def findoddo(start, stop):
+    if stop is None:
+        return findod(start)
     print start, stop
     nasz = c.find({'timestamp': {'$gte': start, '$lt': stop}},
                   {"_id": 0}).sort("timestamp", 1)
-    return [k for k in nasz]
+    return nasz
+
+
+def find_opening_closing(nasz, start, stop):
+    przedzial = nasz.find({'timestamp': {'$gte': start, '$lt': stop}})
+    pocz = przedzial.limit(1).sort("timestamp", 1)
+    konc = przedzial.limit(1).sort("timestamp", -1)
+    return {"pocz": pocz, "konc": konc, "przedzial": przedzial}
+
+
+def find_stations(przedzial):
+    stacje = przedzial.find({}, {"list.loc": 1, "list.info": 1,
+                                 "list.sta": 0, "_id": 0, "timestamp": 0})
+    return set(k for k in stacje)
+
+wszystkie = findall()
+
+print find_stations(wszystkie)
